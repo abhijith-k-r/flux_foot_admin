55ff522af73ce0865d285bf74b9c24d9e524de97
Abhijit <abhijithrono007@gmail.com>
2025-09-12 19:50:08 +0530

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flux_foot_admin/features/auth/presentation/screens/login_screen.dart';
+import 'package:flux_foot_admin/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -13,10 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FluxFoot_Admin',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFF1E2A44),
-     
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xFF1E2A44)),
       home: LogingScreen(),
     );
   }
